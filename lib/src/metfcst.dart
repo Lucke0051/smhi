6c9863822dfb9b49c76443c46f94805d8422d7e3
Lucke <30857678+Lucke0051@users.noreply.github.com>
2021-09-06 12:09:04 +0200

    extension

--- lib/src/metfcst.dart
+++ lib/src/metfcst.dart
@@ -16,8 +16,7 @@ class MeteorologicalForecasts {
 
   ///The current forecast approved time (the time the forecast latest was updated).
   Future<DateTime?> get approvedTime async {
-    final json = await request(constructSmhiUri(
-        metfcstHost, category, version, ["approvedtime.json"]));
+    final json = await request(constructSmhiUri(metfcstHost, category, version, ["approvedtime.json"]));
     if (json != null) {
       return DateTime.tryParse(json["referenceTime"]);
     }
@@ -25,8 +24,7 @@ class MeteorologicalForecasts {
 
   ///The valid times for the current forecast. In the answer you get the valid time list.
   Future<List<DateTime>?> get validTime async {
-    final json = await request(
-        constructSmhiUri(metfcstHost, category, version, ["validtime.json"]));
+    final json = await request(constructSmhiUri(metfcstHost, category, version, ["validtime.json"]));
     if (json != null) {
       final List<DateTime> validTime = List.empty(growable: true);
       for (final String time in json["validTime"]) {
@@ -39,8 +37,7 @@ class MeteorologicalForecasts {
 
   ///The valid times for the current multipoint forecast. In the answer you get the valid time list. You can use the list to specify what valid time when getting a [MultiPointForecast] with [multiPointForecast].
   Future<List<DateTime>?> get multiPointValidTime async {
-    final json = await request(constructSmhiUri(metfcstHost, category, version,
-        ["geotype", "multipoint", "validtime.json"]));
+    final json = await request(constructSmhiUri(metfcstHost, category, version, ["geotype", "multipoint", "validtime.json"]));
     if (json != null) {
       final List<DateTime> validTime = List.empty(growable: true);
       for (final String time in json["validTime"]) {
@@ -53,10 +50,7 @@ class MeteorologicalForecasts {
 
   ///The bounding box polygon for the [Forecast] and [MultiPointForecast] area.
   Future<List<GeoPoint>?> getSMHIBounds() async {
-    final String? data = await request(
-        constructSmhiUri(metfcstHost, Category.pmp3g, Version.two,
-            ["geotype", "polygon.json"]),
-        decode: false);
+    final String? data = await request(constructSmhiUri(metfcstHost, Category.pmp3g, Version.two, ["geotype", "polygon.json"]), decode: false);
     if (data != null) {
       return parseGeoJson(data);
     }
@@ -75,8 +69,7 @@ class MeteorologicalForecasts {
   ///}
   ///```
   Future<Forecast?> forecast(GeoPoint point) async {
-    final json =
-        await request(constructSmhiUri(metfcstHost, category, version, [
+    final json = await request(constructSmhiUri(metfcstHost, category, version, [
       "geotype",
       "point",
       "lon",
@@ -95,13 +88,11 @@ class MeteorologicalForecasts {
   ///The parameter [downsample] allows an integer between 0-20. A downsample value of 2 means that every other value horizontally and vertically is displayed.
   ///
   ///`points` are automatically stored in the [SMHICache] if there is no `points` there already.
-  Future<MultiPointForecast?> multiPointForecast(DateTime validTime,
-      MetFcstParameter parameter, MetFcstLevelType levelType, int level,
+  Future<MultiPointForecast?> multiPointForecast(DateTime validTime, MetFcstParameter parameter, MetFcstLevelType levelType, int level,
       {int? downsample}) async {
     final SMHICache cache = SMHICache();
     final DateFormat formatter = DateFormat("yyyyMMdd'T'HHmmss'Z'");
-    final json =
-        await request(constructSmhiUri(metfcstHost, category, version, [
+    final json = await request(constructSmhiUri(metfcstHost, category, version, [
       "geotype",
       "multipoint",
       "validtime",
@@ -119,10 +110,8 @@ class MeteorologicalForecasts {
     }));
     if (json != null) {
       if (json["geometry"] != null) {
-        cache.metFcstPoints = List.generate(
-            json["geometry"]["coordinates"].length,
-            (int index) => GeoPoint(json["geometry"]["coordinates"][index][1],
-                json["geometry"]["coordinates"][index][0]));
+        cache.metFcstPoints = List.generate(json["geometry"]["coordinates"].length,
+            (int index) => GeoPoint(json["geometry"]["coordinates"][index][1], json["geometry"]["coordinates"][index][0]));
       }
       return MultiPointForecast.fromJson(json);
     }
@@ -145,18 +134,13 @@ class Forecast {
   factory Forecast.fromJson(json) => Forecast(
         approvedTime: DateTime.parse(json["approvedTime"]),
         referenceTime: DateTime.parse(json["referenceTime"]),
-        point: GeoPoint(json["geometry"]["coordinates"][0][1],
-            json["geometry"]["coordinates"][0][0]),
-        timeSeries: List.generate(json["timeSeries"].length,
-            (int index) => ForecastMoment.fromJson(json["timeSeries"][index])),
+        point: GeoPoint(json["geometry"]["coordinates"][0][1], json["geometry"]["coordinates"][0][0]),
+        timeSeries: List.generate(json["timeSeries"].length, (int index) => ForecastMoment.fromJson(json["timeSeries"][index])),
       );
 
   ///Returns the [ForecastMoment] closest to the specified [date].
-  ForecastMoment momentWhen(DateTime date) =>
-      timeSeries.reduce((ForecastMoment value, ForecastMoment element) =>
-          value.validTime.difference(date) < element.validTime.difference(date)
-              ? value
-              : element);
+  ForecastMoment momentWhen(DateTime date) => timeSeries.reduce(
+      (ForecastMoment value, ForecastMoment element) => value.validTime.difference(date) < element.validTime.difference(date) ? value : element);
 }
 
 //Forecasts are divided into moments where each [ForecastMoment] represents a timestamp.
@@ -209,9 +193,7 @@ class MultiPointForecast {
     return MultiPointForecast(
       approvedTime: DateTime.parse(json["approvedTime"]),
       referenceTime: DateTime.parse(json["referenceTime"]),
-      values: json["timeSeries"] != null && json["timeSeries"].isNotEmpty
-          ? json["timeSeries"][0]["parameters"][0]["values"]
-          : [],
+      values: json["timeSeries"] != null && json["timeSeries"].isNotEmpty ? json["timeSeries"][0]["parameters"][0]["values"] : [],
     );
   }
 
@@ -222,8 +204,7 @@ class MultiPointForecast {
     double? closestDistance;
     for (int i = 0; i < points.length; i++) {
       final GeoPoint point = points[i];
-      final double distance = calculateLatLongDistance(
-          at.latitude, at.longitude, point.latitude, point.longitude);
+      final double distance = calculateLatLongDistance(at.latitude, at.longitude, point.latitude, point.longitude);
       if ((closestIndex == null) || (closestDistance! > distance)) {
         closestIndex = i;
         closestDistance = distance;
@@ -357,6 +338,53 @@ extension MetFcstParameterExtension on MetFcstParameter {
         return Unit.code;
       default:
         return Unit.celcius;
+    }
+  }
+}
+
+extension MetFcstExtension on String {
+  MetFcstParameter toMetFcstParameter() {
+    switch (this) {
+      case "msl":
+        return MetFcstParameter.airPressure;
+      case "t":
+        return MetFcstParameter.airTemperature;
+      case "vis":
+        return MetFcstParameter.horizontalVisibility;
+      case "wd":
+        return MetFcstParameter.windDirection;
+      case "ws":
+        return MetFcstParameter.windSpeed;
+      case "r":
+        return MetFcstParameter.relativeHumidity;
+      case "tstm":
+        return MetFcstParameter.thunderProbability;
+      case "tcc_mean":
+        return MetFcstParameter.meanValueOfTotalCloudCover;
+      case "lcc_mean":
+        return MetFcstParameter.meanValueOfLowLevelCloudCover;
+      case "mcc_meam":
+        return MetFcstParameter.meanValueOfMediumLevelCloudCover;
+      case "hcc_mean":
+        return MetFcstParameter.meanValueOfHighLevelCloudCover;
+      case "gust":
+        return MetFcstParameter.windGustSpeed;
+      case "pmin":
+        return MetFcstParameter.minimumPrecipitationIntensity;
+      case "pmax":
+        return MetFcstParameter.maximumPrecitipationIntensity;
+      case "spp":
+        return MetFcstParameter.percentOfPrecipitationInFrozenForm;
+      case "pcat":
+        return MetFcstParameter.precipitationCategory;
+      case "pmean":
+        return MetFcstParameter.meanPrecitipationIntensity;
+      case "pmedian":
+        return MetFcstParameter.medianPrecitipationIntensity;
+      case "Wsymb2":
+        return MetFcstParameter.weatherSymbol;
+      default:
+        return MetFcstParameter.airTemperature;
     }
   }
 }
